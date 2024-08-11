fdd4c984514090058f18c908e213c6d8907c9a93
Ammar46421 <153704851+Ammar46421@users.noreply.github.com>
2024-08-11 21:11:18 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-// function tellTime() {
-//     var now = new Date();
-//     var theHr = now.getHours();
-//     var theMin = now.getMinutes();
-//     alert("Current time: " + theHr + ":" + theMin);
-// }
+function(){
 
-
-function tellTime() {
-    var now = new Date();
-    var theHr = now.getHours();
-    var theMin = now.getMinutes();
-    alert("current time: "+ theHr + ":" + theMin);
 }
-
-console.log(tellTime)
-
-function greetUser() {
-    alert("hello")
-}
-
-let todaysDate = new Date();
-let dateInString = todaysDate.toString();
-
-// document.write(`<div class="card">
-// <h4>Printing Today's Date in Browser</h4>
-// <span>${dateInString}</span>
-// </div>`);
-
-document.write(`<div class="card"> 
-    <h4>Printing Today's Date in Browser</h4>
-    <span>${dateInString}</span>
-    </div>`);
